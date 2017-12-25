90e373ca87795dc7a31a292f9e715266cd021452
Mark Hamstra <hello@markhamstra.com>
2017-12-25 01:13:08 +0100

    Add translations via crowdin (https://crowdin.com/project/modmore-commerce)

---
+++ core/components/commerce_randomlypricedproduct/lexicon/nl/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$_lang['commerce_rpp.randomlypricedproduct'] = 'Product met willekeurige prijs';
+$_lang['commerce_rpp.randomlypricedproduct.description'] = 'Activeer deze module om een product met willekeurige prijs beschikbaar te maken voor het beheren van producten.';
+$_lang['commerce_rpp.min_price'] = 'Minimale prijs';
+$_lang['commerce_rpp.max_price'] = 'Maximale prijs';
+$_lang['commerce_rpp.tv_description'] = 'Dit product heeft een <b>willekeurige prijs</b> tussen <b>[[+min]] en [[+max]]</b>.';
+
+// Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
+$_lang['commerce.RandomlyPricedProduct'] = 'Product met willekeurige prijs';
+$_lang['commerce.add_RandomlyPricedProduct'] = 'Product met willekeurige prijs toevoegen';
---
+++ core/components/commerce_randomlypricedproduct/lexicon/sv/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$_lang['commerce_rpp.randomlypricedproduct'] = 'Randomly Priced Product';
+$_lang['commerce_rpp.randomlypricedproduct.description'] = 'Enable this module to make the Randomly Priced Product available when managing products.';
+$_lang['commerce_rpp.min_price'] = 'Minimum Price';
+$_lang['commerce_rpp.max_price'] = 'Maximum Price';
+$_lang['commerce_rpp.tv_description'] = 'This product has a <b>random price</b> between <b>[[+min]] and [[+max]]</b>.';
+
+// Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
+$_lang['commerce.RandomlyPricedProduct'] = 'Randomly Priced Product';
+$_lang['commerce.add_RandomlyPricedProduct'] = 'Add Randomly Priced Product';
