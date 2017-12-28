67ea6f33a12e069865ab69de7678636f6eb43faf
Mark Hamstra <hello@markhamstra.com>
2017-12-28 15:47:57 +0100

    Sync translations and add Indonesian per request on Crowdin

--- core/components/commerce_randomlypricedproduct/lexicon/de/default.inc.php
+++ core/components/commerce_randomlypricedproduct/lexicon/de/default.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-$_lang['commerce_rpp.randomlypricedproduct'] = 'Randomly Priced Product';
-$_lang['commerce_rpp.randomlypricedproduct.description'] = 'Enable this module to make the Randomly Priced Product available when managing products.';
-$_lang['commerce_rpp.min_price'] = 'Minimum Price';
-$_lang['commerce_rpp.max_price'] = 'Maximum Price';
-$_lang['commerce_rpp.tv_description'] = 'This product has a <b>random price</b> between <b>[[+min]] and [[+max]]</b>.';
+$_lang['commerce_rpp.randomlypricedproduct'] = 'Nach Zufallsprinzip ausgezeichnetes Produkt';
+$_lang['commerce_rpp.randomlypricedproduct.description'] = 'Aktivieren Sie dieses Modul um nach dem Zufallsprinzip ausgezeichnete Produkte zur Verfügung stellen, wenn Sie Produkte verwalten.';
+$_lang['commerce_rpp.min_price'] = 'Mindestpreis';
+$_lang['commerce_rpp.max_price'] = 'Höchstpreis';
+$_lang['commerce_rpp.tv_description'] = 'Dieses Produkt hat einen <b>zufälligen Preis</b> zwischen <b>[[+min]] und [[+max]]</b>.';
 
 // Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
-$_lang['commerce.RandomlyPricedProduct'] = 'Randomly Priced Product';
-$_lang['commerce.add_RandomlyPricedProduct'] = 'Add Randomly Priced Product';
+$_lang['commerce.RandomlyPricedProduct'] = 'Nach Zufallsprinzip ausgezeichnetes Produkt';
+$_lang['commerce.add_RandomlyPricedProduct'] = 'Nach Zufallsprinzip ausgezeichnetes Produkt hinzufügen';
--- core/components/commerce_randomlypricedproduct/lexicon/es/default.inc.php
+++ core/components/commerce_randomlypricedproduct/lexicon/es/default.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-$_lang['commerce_rpp.randomlypricedproduct'] = 'Randomly Priced Product';
-$_lang['commerce_rpp.randomlypricedproduct.description'] = 'Enable this module to make the Randomly Priced Product available when managing products.';
-$_lang['commerce_rpp.min_price'] = 'Minimum Price';
-$_lang['commerce_rpp.max_price'] = 'Maximum Price';
-$_lang['commerce_rpp.tv_description'] = 'This product has a <b>random price</b> between <b>[[+min]] and [[+max]]</b>.';
+$_lang['commerce_rpp.randomlypricedproduct'] = 'Producto a un precio al azar';
+$_lang['commerce_rpp.randomlypricedproduct.description'] = 'Activa este módulo para hacer Producto a un Precio al Azar disponible cuando administres productos.';
+$_lang['commerce_rpp.min_price'] = 'Precio mínimo';
+$_lang['commerce_rpp.max_price'] = 'Precio máximo';
+$_lang['commerce_rpp.tv_description'] = 'Este producto tiene un <b>precio al azar</b> entre <b>[[+min]] y [[+max]]</b>.';
 
 // Some lexicons are automatically called by the commerce core, based on the product class name, so add those.
-$_lang['commerce.RandomlyPricedProduct'] = 'Randomly Priced Product';
-$_lang['commerce.add_RandomlyPricedProduct'] = 'Add Randomly Priced Product';
+$_lang['commerce.RandomlyPricedProduct'] = 'Producto a un precio al azar';
+$_lang['commerce.add_RandomlyPricedProduct'] = 'Añadir Producto a un precio al azar';
